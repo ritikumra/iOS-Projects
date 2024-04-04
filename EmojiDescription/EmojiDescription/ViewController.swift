08aad4c81536a818ee3c218e0a664175d0a6c55d
Riti Kumra <102514662+ritikumra@users.noreply.github.com>
2024-04-04 14:51:07 +0530

    Add files via upload

---
+++ EmojiDescription/EmojiDescription/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDescription
+//
+//  Created by student on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
