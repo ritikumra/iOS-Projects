eb072d8cd713276bc17c31997bf4225327b0299c
Riti Kumra <102514662+ritikumra@users.noreply.github.com>
2024-04-04 14:49:00 +0530

    Add files via upload

---
+++ Rainbowtabs/Rainbowtabs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rainbowtabs2
+//
+//  Created by student on 04/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
